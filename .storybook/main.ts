45a9837864ab4d928420eefc67c3fe07ce09f5fd
marcus <marcus.koch01@outlook.de>
2023-04-23 20:49:46 +0200

    feat: Testing Modules in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,19 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-a11y',
+  ],
   framework: {
     name: '@storybook/react-vite',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: 'tag'
-  }
-};
-export default config;
+    autodocs: 'tag',
+  },
+}
+export default config
